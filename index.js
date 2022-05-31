7bc6b23235a2c47318f9f63a77ef5179bc16cd55
Rohail Babar <rohailbabar1997@gmail.com>
2022-05-31 19:26:19 +0500

    Inital Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const functions = require('@google-cloud/functions-framework');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+functions.http('create', async (req, res) => {
+    const customer = await stripe.customers.create({
+        description: 'My First Test Customer (created for API docs at https://www.stripe.com/docs/api)',
+    });
+    res.json(customer)
+});
